2654c43e4c4a84a6d285e771e58a8a9b709037a7
Fabio <fabioramosnic@gmail.com>
2023-05-08 03:21:50 -0300

    Cambio en las cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,8 +9,8 @@ function Card() {
                         <img src="https://img.freepik.com/foto-gratis/camisetas-negras-espacio-copia_53876-102012.jpg?w=1380&t=st=1682926194~exp=1682926794~hmac=bc83065a8ce5a88aa30265724e8da957153f3762f818335a2bb548cb5c2fbb28" alt="" />
                     </div>
                     <div className="card_body">
-                        <p>$30.99</p> <br />
-                        <p>Remera muy pro</p>
+                        <p>$3000</p> <br />
+                        <p>Remera Negra</p>
                     </div>
                 </div>
             </a>
@@ -20,8 +20,8 @@ function Card() {
                         <img src="https://img.freepik.com/foto-gratis/camisetas-blancas-espacio-copia-sobre-fondo-gris_53876-104920.jpg?w=1380&t=st=1682926206~exp=1682926806~hmac=df50a4c73f005bed2e906c68c9a94362f282c62f895aa7ca1ab6229290eef310" alt="" />
                     </div>
                     <div className="card_body">
-                        <p>$30.99</p> <br />
-                        <p>Remera muy pro</p>
+                        <p>$3000</p> <br />
+                        <p>Remera Blanca</p>
                     </div>
                 </div>
             </a>
@@ -31,8 +31,8 @@ function Card() {
                         <img src="https://img.freepik.com/foto-gratis/vista-gorra-camionero-equipo-beisbol_23-2149410056.jpg?w=1380&t=st=1682926264~exp=1682926864~hmac=18874b50fdcb5913fdcbc66f7959fc5c32978d564cf9253341b6d7afa13d68bb" alt="" />
                     </div>
                     <div className="card_body">
-                        <p>$30.99</p> <br />
-                        <p>Remera muy pro</p>
+                        <p>$12000</p> <br />
+                        <p>Conjunto</p>
                     </div>
                 </div>
             </a>
